e13d7abb4f44e708b2a87cd062e42901223645c1
David Gallo <david.gallo747@gmail.com>
2021-05-14 23:18:14 -0400

    minor copy changes

--- !v2/assets/js/professional.js
+++ !v2/assets/js/professional.js
@@ -24,7 +24,7 @@ const items = document.querySelectorAll('.item'),
   headerItems = document.querySelectorAll('.item-header'),
   descriptionItems = document.querySelectorAll('.item-description'),
   activeDelay = .76,
-  interval = 5000;
+  interval = 10000;
 
 let current = 0;
 
